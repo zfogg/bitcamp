565ea44a9036b80bd62d4baa6b8f2fd3b6405cbc
Zach Fogg <zach.fogg@gmail.com>
2013-11-13 01:25:33 -0500

    Initial bitcamp commit.

---
+++ compass.config.rb
@@ -0,0 +1,8 @@
+http_path = "public/"
+css_dir = "public/css"
+sass_dir = "src/public/sass"
+images_dir = "public/images"
+javascripts_dir = "public/js"
+output_style = :compressed
+relative_assets = true
+line_comments = false
